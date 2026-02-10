2670d3a35e2e7c5812b30d21cef4c1841938c615
roie13 <162023262+roie13@users.noreply.github.com>
2026-02-10 21:23:30 +0200

    Fix NuGet package version for .NET 8.0

--- obj/Debug/net8.0/KickRateServer.AssemblyInfo.cs
+++ obj/Debug/net8.0/KickRateServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KickRateServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9500dbfc7dda157f0e5bdbbf7a679bfe7d45fa49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fa7907e56e8bb464f05fcaa073b1917b2ee47a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("KickRateServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KickRateServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
